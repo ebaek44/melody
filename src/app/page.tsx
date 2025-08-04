a3842d1caaa045bd968cb9e05f5965f3828d861f
Ethan <ethanbaek2005@gmail.com>
2025-08-03 21:54:55 -0700

    Messed up a lot of stuff trying the PR but reverted it and keeping it on the login branch for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "@/components/HeroSection";
-
+import Header from "@/components/Header";
 /// Home page
 export default function Home() {
   return (
     <div className="pt-24">
+      <Header />
       <HeroSection />
     </div>
   );
