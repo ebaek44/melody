40abb449c5a18053263639cea397e2f5cc250834
Ethan <ethanbaek2005@gmail.com>
2025-10-02 15:27:41 -0700

    added the node web basic animation

--- src/components/node-map/node.tsx
+++ src/components/node-map/node.tsx
@@ -1,31 +1,20 @@
-// Make an interface for the props
-interface ArtistNodeProps {
-  artistName: string;
-  artistURL: string;
-  bgPfp?: string;
-}
+import { Artist } from "@/types";
 
 // This object will have the artist name and the url to get to their page
-export default function Node({
-  artistName,
-  artistURL,
-  bgPfp,
-}: ArtistNodeProps) {
+export default function Node(a: Artist) {
   return (
     <a
-      target="_blank"
-      href={artistURL}
       className="
-  w-25 h-25 rounded-full flex justify-center 
+  relative block w-[6rem] h-[6rem] rounded-full flex justify-center 
   border-solid border-gray-300 border-[5px] 
   transform-gpu transition-transform duration-200 ease-out hover:scale-105 active:scale-95 
   bg-cover bg-no-repeat bg-[position:center_30%]"
-      style={{ backgroundImage: bgPfp ? `url(${bgPfp})` : undefined }}
+      style={{ backgroundImage: a.pfp ? `url(${a.pfp})` : undefined }}
     >
-      {!bgPfp && (
+      {!a.pfp && (
         <div className="absolute inset-0 flex items-center justify-center rounded-full bg-green-300">
           <span className="px-2 text-center text-sm text-black leading-tight break-words">
-            {artistName}
+            {a.name}
           </span>
         </div>
       )}
---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+export type Artist = {
+  name: string;
+  url: string;
+  id: string;
+  followers?: number;
+  popularity?: number;
+  pfp?: string;
+};
