593258fe951a1e4aa303a4c853251ded6f3152b5
Ethan <ethanbaek2005@gmail.com>
2025-10-06 16:21:40 -0700

    fa

--- src/app/api_test/page.tsx
+++ src/app/api_test/page.tsx
@@ -4,11 +4,5 @@ import { fetchRelatedArtists } from "../actions/lastfm/actions";
 export default async function Page() {
   const data = await fetchRelatedArtists("Taylor Swift");
   const list = data?.similarartists?.artist ?? [];
-  return (
-    <div>
-      {list.map((a: any) => (
-        <div key={a.name}>{a.name}</div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 }
